1548a645226430ea9754ad7bfc5b3a611883f10e
Natasha S Love <debora@inbox.ru>
2018-11-15 17:59:08 -0600

    Add project files.

---
+++ nLove_inclass10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nLove_inclass10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
