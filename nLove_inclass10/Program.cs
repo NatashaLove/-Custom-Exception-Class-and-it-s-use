4f1b7d88f0e22706bbc1c949ea0752c593f9d4bf
Natasha S Love <debora@inbox.ru>
2018-11-15 18:36:25 -0600

    Exceptions

--- nLove_inclass10/Program.cs
+++ nLove_inclass10/Program.cs
@@ -9,6 +9,40 @@ namespace nLove_inclass10
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Trip t = new Trip();
+
+                try
+                {
+                    Console.WriteLine("How many miles did you go?");
+                    t.Miles = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("How many gallons did you use?");
+                    t.Gallons = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("You got about {0} miles per gallon.", t.MPG);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Can't divide by zero!");
+                }
+                finally
+                {
+                    Console.WriteLine("Thanks for playing!");
+                }
+
+               
+                Console.Write("Try again?");
+                if (Console.ReadLine().ToUpper() == "N")
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("Press any button..");
+            Console.ReadKey();
+              
         }
     }
 }
---
+++ nLove_inclass10/Trip.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nLove_inclass10
+{
+    class Trip
+    {
+        private int _miles;
+        private int _gallons;
+
+        public Trip() {
+
+            _miles = 0;
+            _gallons = 0;
+        }
+
+        //extended property
+
+        public int Miles {
+            get { return _miles; }
+            set { _miles = value; }
+        }
+
+        public int Gallons {
+            get { return _gallons; }
+            set { _gallons = value; }
+        }
+
+        public int MPG { get { return Miles / Gallons; } }
+    }
+}
